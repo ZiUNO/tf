789d6c26fe9235fe4c62e6b0aa27131b215728eb
ZiUNO <331245059@qq.com>
2018-08-08 13:27:05 +0800

    Add: import os os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' Or: make tensorflow code To solve problem: Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA

--- AlexNet.py
+++ AlexNet.py
@@ -2,117 +2,144 @@ from datetime import datetime
 import math
 import time
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+'''
+8个训练参数层
+前5层为卷积层
+后三层为全连接层
+最后一层：1000类输出的Softmax层用作分类
+超参数：
+    1.卷积层 卷积核11x11 步长4 96卷积核
+    2.LRN层
+    3.最大池化层3x3 步长2
+    4+.卷积层5x5/3x3 步长1 最大池化层3x3 步长2
+    
+'''
 
 batch_size = 32
 num_batches = 100
 
+
+# 显示网络每一层结构 张量名和张量尺寸
 def print_activations(t):
-	print(t.op.name, ' ', t.get_shape().as_list())
+    print(t.op.name, ' ', t.get_shape().as_list())
 
+
+# 接受image作为出入
+# 返回最后一层pool5及parameters（所有需要训练的模型参数）
 def inference(images):
-	parameters = []
+    parameters = []
 
-	with tf.name_scope('conv1') as scope:
-		kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 64], dtype = tf.float32, stddev = 1e-1), name = 'weights')
-		conv = tf.nn.conv2d(images, kernel, [1, 4, 4, 1], padding = 'SAME')
-		biases = tf.Variable(tf.constant(0.0, shape = [64], dtype = tf.float32), trainable = True, name = 'biases')
-		bias = tf.nn.bias_add(conv, biases)
-		conv1 = tf.nn.relu(bias, name = scope)
-		print_activations(conv1)
-		parameters += [kernel, biases]
+    with tf.name_scope('conv1') as scope:
+        kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 64], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(images, kernel, [1, 4, 4, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope)
+        print_activations(conv1)
+        parameters += [kernel, biases]
 
-	lrn1 = tf.nn.lrn(conv1, 4, bias = 1.0, alpha = 0.001 / 9, beta = 0.75, name = 'lrn1')
-	pool1 = tf.nn.max_pool(lrn1, ksize = [1, 3, 3, 1], strides = [1, 2, 2, 1], padding = 'VALID', name = 'pool1')
-	print_activations(pool1)
+    lrn1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn1')
+    pool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool1')
+    print_activations(pool1)
 
-	with tf.name_scope('conv2') as scope:
-		kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 192], dtype = tf.float32, stddev = 1e-1), name = 'weights')
-		conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding = 'SAME')
-		biases = tf.Variable(tf.constant(0.0, shape = [192], dtype = tf.float32), trainable = True, name = 'biases')
-		bias = tf.nn.bias_add(conv, biases)
-		conv2 = tf.nn.relu(bias, name = scope)
-		parameters += [kernel, biases]
-	print_activations(conv2)
+    with tf.name_scope('conv2') as scope:
+        kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 192], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[192], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+    print_activations(conv2)
 
-	lrn2 = tf.nn.lrn(conv2, 4, bias = 1.0, alpha = 0.001 / 9, beta = 0.75, name = 'lrn2')
-	pool2 = tf.nn.max_pool(lrn2, ksize = [1, 3, 3, 1], strides = [1, 2, 2, 1], padding = 'VALID', name = 'pool2')
-	print_activations(pool2)
+    lrn2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn2')
+    pool2 = tf.nn.max_pool(lrn2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool2')
+    print_activations(pool2)
 
-	with tf.name_scope('conv3') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 1, 192, 384], dtype = tf.float32, stddev = 1e-1), name = 'weights')
-		conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding = 'SAME')
-		biases = tf.Variable(tf.constant(0.0, shape = [384], dtype = tf.float32), trainable = True, name = 'biases')
-		bias = tf.nn.bias_add(conv, biases)
-		conv3 = tf.nn.relu(bias, name = scope)
-		parameters += [kernel, biases]
-		print_activations(conv3)
+    with tf.name_scope('conv3') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 192, 384], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv3 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+        print_activations(conv3)
 
-	with tf.name_scope('conv4') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256], dtype = tf.float32, stddev = 1e-1), name = 'weights')
-		conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding = 'SAME')
-		biases = tf.Variable(tf.constant(0.0, shape = [256], dtype = tf.float32), trainable = True, name = 'biases')
-		bias = tf.nn.bias_add(conv, biases)
-		conv4 = tf.nn.relu(bias, name = scope)
-		parameters += [kernel, biases]
-		print_activations(conv4)
+    with tf.name_scope('conv4') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv4 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+        print_activations(conv4)
 
-	with tf.name_scope('conv5') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 256], dtype = tf.float32, stddev = 1e-1), name = 'weights')
-		conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding = 'SAME')
-		biases = tf.Variable(tf.constant(0.0, shape = [256], dtype = tf.float32), trainable = True, name = 'biases')
-		bias = tf.nn.bias_add(conv, biases)
-		conv5 = tf.nn.relu(bias, name = scope)
-		parameters += [kernel, biases]
-		print_activations(conv5)
+    with tf.name_scope('conv5') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv5 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+        print_activations(conv5)
 
-	pool5 = tf.nn.max_pool(conv5, ksize = [1, 3, 3, 1], strides = [1, 2, 2, 1], padding = 'VALID', name = 'pool5')
-	print_activations(pool5)
+    pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool5')
+    print_activations(pool5)
 
-	return pool5, parameters
+    return pool5, parameters
+
+
+'''
+评估每轮计算时间
+参数：
+    session
+    需要评测的运算算子
+    测试名称
+'''
+
 
 def time_tensorflow_run(session, target, info_string):
-	num_steps_burn_in = 10
-	total_duration = 0.0
-	total_duration_squared = 0.0
+    # num_step_burn_in 预热轮数
+    num_steps_burn_in = 10
+    total_duration = 0.0  # 总时间
+    total_duration_squared = 0.0
 
-	for i in range(num_batches + num_steps_burn_in):
-		start_time = time.time()
-		_ = session.run(target)
-		duration = time.time() - start_time
-		if i >= num_steps_burn_in:
-			if not i % 10:
-				print('%s: step %d, duration = %.3f' % (datetime.now(), i - num_steps_burn_in, duration))
-				total_duration += duration
-				total_duration_squared += duration * duration
+    for i in range(num_batches + num_steps_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_steps_burn_in:
+            if not i % 10:
+                print('%s: step %d, duration = %.3f' % (datetime.now(), i - num_steps_burn_in, duration))
+                total_duration += duration
+                total_duration_squared += duration * duration
 
-	mn = total_duration / num_batches
-	vr = total_duration_squared / num_batches - mn * mn
-	sd = math.sqrt(vr)
-	print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' % (datetime.now(), info_string, num_batches, mn, sd))
+    mn = total_duration / num_batches
+    vr = total_duration_squared / num_batches - mn * mn
+    sd = math.sqrt(vr)
+    print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' % (datetime.now(), info_string, num_batches, mn, sd))
+
 
 def run_benchmark():
-	with tf.Graph().as_default():
-		images_size = 224
-		images = tf.Variable(tf.random_normal([batch_size, images_size, images_size, 3], dtype = tf.float32, stddev = 1e-1))
+    with tf.Graph().as_default():
+        images_size = 224
+        images = tf.Variable(tf.random_normal([batch_size, images_size, images_size, 3], dtype=tf.float32, stddev=1e-1))
 
-		pool5, parameters = inference(images)
+        pool5, parameters = inference(images)
 
-		init = tf.global_variables_initializer()
-		sess = tf.Session()
-		sess.run(init)
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
 
-		time_tensorflow_run(sess, pool5, "Forward")
+        time_tensorflow_run(sess, pool5, "Forward")
 
-		objective = tf.nn.l2_loss(pool5)
-		grad = tf.gradients(objective, parameters)
-		time_tensorflow_run(sess, grad, "Forward-backward")
+        objective = tf.nn.l2_loss(pool5)
+        grad = tf.gradients(objective, parameters)
+        time_tensorflow_run(sess, grad, "Forward-backward")
+
 
 run_benchmark()
 
 input('Done!')
-
-
-
-
-
-
